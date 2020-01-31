4a21af585c9d6a9d15e367f73e38eb72b62e6724
Dimeone1 <welhamnathan1@gmail.com>
2020-01-31 20:55:10 +0000

    Menu Scene
    Implemented bound boxes to determine which button the player is currently interacting with.

---
+++ TheVanGorps2020/Assets/Scripts/Collision/InteractableBoundary.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableBoundary : MonoBehaviour
+{
+    private string name;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        name = other.name;
+        Debug.Log("Entered " + name);
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        bool keyPressed = Input.GetKeyDown(KeyCode.E);
+        if(keyPressed)
+        {
+            switch(name)
+            {
+                case "boundPlay":
+                    Debug.Log("Hit button in play");
+                    break;
+
+                case "boundSetting":
+                    Debug.Log("Hit button in play");
+                    break;
+
+                case "boundExit":
+                    Debug.Log("Hit button in play");
+                    break;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Left " + name);
+    }
+}
