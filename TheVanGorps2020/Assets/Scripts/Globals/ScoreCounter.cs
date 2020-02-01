d00f920d0b54ec24d1d730e4d1d603ce8079ec8b
Nathan <nathandewhurst1998@gmail.com>
2020-02-01 12:56:32 +0000

    Added globals script

---
+++ TheVanGorps2020/Assets/Scripts/Globals/ScoreCounter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreCounter : MonoBehaviour
+{
+    /// <summary>
+    /// This script just needs to be applied to an object in each scene and can then be accessed.
+    /// </summary>
+    static int GlobalTimer;
+}
