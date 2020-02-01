d5be2204918a7927c066d58648a4b240d645f120
jlbw96 <jlbw96@hotmail.co.uk>
2020-02-01 15:15:33 +0000

    Added overlay for camera
    Includes inventory system and timer overlay.

--- TheVanGorps2020/Assets/Scripts/Globals/ScoreCounter.cs
+++ TheVanGorps2020/Assets/Scripts/Globals/ScoreCounter.cs
@@ -1,11 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreCounter : MonoBehaviour
 {
+    public Text timerText;
     /// <summary>
     /// This script just needs to be applied to an object in each scene and can then be accessed.
     /// </summary>
-    static int GlobalTimer;
+    static float GlobalTimer;
+    int rounded_timer;
+
+
+    void Start()
+    {
+        //timerText.text = GlobalTimer.ToString();
+
+    }
+
+
+    void Update()
+    {
+        GlobalTimer += 1 * Time.deltaTime;
+        rounded_timer = (int)(GlobalTimer+0.5f);
+        Debug.Log(rounded_timer);
+        timerText.text = rounded_timer.ToString();
+    }
 }
